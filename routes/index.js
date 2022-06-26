d55fa0d02aae2b47e1d8033bcf3e830aaffb0951
LeonAngelit <leo63582@gmail.com>
2022-06-26 10:53:25 +0200

    hello

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const cousesRouter = require('./courses');
+const coursesRouter = require('./courses');
 const express = require('express');
 
 function routerAPI(app) {
   const router = express.Router();
   app.use('/api/v1', router);
-  router.use('/courses', cousesRouter);
+  router.use('/courses', coursesRouter);
 }
 
 module.exports = routerAPI;
