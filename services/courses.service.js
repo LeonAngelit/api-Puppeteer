e57ffd3e753edd71d45d28933c0233927d77edca
LeonAngelit <leo63582@gmail.com>
2026-01-02 21:09:50 +0100

    Adapt logic to api pagination and properties

--- services/courses.service.js
+++ services/courses.service.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const res = require("express/lib/response");
 
 //Creamos la clase que instanciaremos en el archivo courses.js
 class CourseService {
@@ -6,18 +7,29 @@ class CourseService {
     this.courses = [];
   }
   async #getCourses(url, userName) {
-    const fullUrl = url + userName + "/";
-    const response = await axios.get(fullUrl);
-      let array = [];
-      response?.data?.courses.map((curso) => {
+    const firstPage = 1;
+    const fullUrl = url + userName + "/?page_size=100&page=";
+    let response = await axios.get(fullUrl + firstPage);
+    const numberOfPages = response?.data?.metadata?.pages;
+    let array = [];
+    response?.data?.data?.courses.map((curso) => {
+      array.push({
+        title: curso.title,
+        image: curso.badge_url,
+        link: curso.diploma.diploma_url,
+      });
+    });
+    for (let i = firstPage + 1; i <= numberOfPages; i++) {
+      response = await axios.get(fullUrl + i);
+      response?.data?.data?.courses.map((curso) => {
         array.push({
           title: curso.title,
-          image: curso.badge,
-          link: curso.diploma_link,
+          image: curso.badge_url,
+          link: curso.diploma?.diploma_url,
         });
       });
-      return array;
-  
+    }
+    return array;
   }
   //Este es el método público que he usado para acceder al privado
   find(url, userName) {
